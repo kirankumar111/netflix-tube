7c91bfea667fa9dccec3ec432d98dfebe8faccba
Kiran Kumar <kiran.kumar@medibuddy.in>
2022-10-24 02:35:36 +0530

    Netflix Tube 1

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { NextScript, Html, Main, Head } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet" />
+                </Head >
+                <body>
+                    <Main></Main>
+                    <NextScript />
+                </body>
+            </Html >
+        );
+    }
+}
+
+export default MyDocument;
